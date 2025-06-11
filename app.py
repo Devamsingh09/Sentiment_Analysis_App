cce9d00cb2ec8b7fb21a78fa030535903604966e
Devam Singh <141946786+Devamsingh09@users.noreply.github.com>
2025-06-11 07:42:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ import pickle
 st.set_page_config(page_title="Sentiment Analysis", layout="centered", initial_sidebar_state="collapsed")
 
 # Load the pre-trained model
-model = load_model('model.keras')
+try:
+    model = load_model('model.keras', compile=False)
+except Exception as e:
+    st.error(f"Failed to load model: {e}")
+    st.stop()
+
 
 # Load the tokenizer from the pickle file
 with open('./tokenizer.pkl', 'rb') as f:
